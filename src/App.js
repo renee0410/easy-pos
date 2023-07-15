ebc86e8de2c15890ab7675ea0f07f6757e734eca
renee0410 <reneewang410@gmail.com>
2023-07-15 17:42:53 +0800

    初始化設定路由

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import './style/all.scss';
+import { Routes, Route } from 'react-router-dom';
+// pages
+import { Layout } from './pages/Layout';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='' element={<Layout></Layout>}>
+          
+        </Route>
+      </Routes>
     </div>
   );
 }
