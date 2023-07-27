46d7da199d4916ee4db8d8378571d49d12f9fc50
renee0410 <reneewang410@gmail.com>
2023-07-27 20:49:33 +0800

    製作計數器功能

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,19 +3,34 @@ import {
   mdiPlus,  // 加
   mdiMinus, // 減
 } from '@mdi/js';
+import { useState } from 'react';
 
 export function Counter() {
+  // 儲存計數器數量
+  const [ num, setNum ] = useState(0);
+
+  function add() {
+    setNum( num + 1 );
+  }
+
+  function minus() {
+    // 限制計數器為負數
+    if (num) {
+      setNum( num - 1 );
+    }
+  }
+
   return (
     <div className="counterBtn">
-    <div className="minusBtn">
+    <div className="minusBtn" onClick={minus}>
         <div className="icon">
           <Icon path={mdiMinus}></Icon>
         </div>
       </div>
       <div className="numBlock">
-        <span>10</span>
+        <span>{num}</span>
       </div>
-      <div className="plusBtn">
+      <div className="plusBtn" onClick={add}>
         <div className="icon">
           <Icon path={mdiPlus}></Icon>
         </div>
