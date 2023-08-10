32166598c28fa29d3954296e1521800ce43d90cb
renee0410 <reneewang410@gmail.com>
2023-08-10 22:36:01 +0800

    加入是否有登入的驗證

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import { SideMenu } from "../components/SideMenu";
 import { Header } from "../components/Header";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export function Layout() {
+  const navigate = useNavigate();
+
+  // 驗證是否登入
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      navigate("/login");
+    }
+  });
+
 	return (
 		<div className="layout">
 			<div className="lBlock">
